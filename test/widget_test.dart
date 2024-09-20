b2ee7147bfab933d47d2e438609e2db7eb60fc2b
Matias Navarro Carter <mnavarrocarter@gmail.com>
2024-09-20 13:03:28 +0100

    chore: build pipeline

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,24 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:snuggy/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('finds all sounds in the app', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that we have all the sounds
+    expect(find.text('Rain'), findsOneWidget);
+    expect(find.text('Storm'), findsOneWidget);
+    expect(find.text('Wind'), findsOneWidget);
+    expect(find.text('Waves'), findsOneWidget);
+    expect(find.text('Stream'), findsOneWidget);
+    expect(find.text('Birds'), findsOneWidget);
+    expect(find.text('Summer Night'), findsOneWidget);
+    expect(find.text('Train'), findsOneWidget);
+    expect(find.text('Boat'), findsOneWidget);
   });
 }

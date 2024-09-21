364a40f321f9c8e9385eff2ac184480107226919
Matias Navarro Carter <mnavarrocarter@gmail.com>
2024-09-21 15:02:46 +0100

    chore: refactor sound widget
    Now the widget receives the props that control playback state, so presets can work properly.

--- lib/sound.dart
+++ lib/sound.dart
@@ -6,10 +6,14 @@ import 'package:audioplayers/audioplayers.dart';
 class SoundWidget extends StatefulWidget {
   final String title;
   final String asset;
+  final double volume;
+  final bool playing;
 
   const SoundWidget({
     required this.title,
     required this.asset,
+    this.volume = 0.2,
+    this.playing = false,
     super.key
   });
 
@@ -19,17 +23,29 @@ class SoundWidget extends StatefulWidget {
 
 class _SoundWidgetState extends State<SoundWidget> {
   final AudioPlayer _player = AudioPlayer();
-  bool _isActive = false;
-  double _volume = 20;
+  final ValueNotifier<bool> _playing = ValueNotifier(false);
+  final ValueNotifier<double> _volume = ValueNotifier(0.2);
 
   @override
   void initState() {
     super.initState();
+    
+    // Register callbacks for AudioPlayer
+    _playing.addListener(_startOrStopPlayback);
+    _volume.addListener(_changePlaybackVolume);
+    
+    // Make the player loop
     _player.setReleaseMode(ReleaseMode.loop);
+    
+    // Initialize the state to the passed widget values
+    _playing.value = widget.playing;
+    _volume.value = widget.volume;
   }
 
   @override
   void dispose() {
+    _playing.dispose();
+    _volume.dispose();
     _player.dispose();
     super.dispose();
   }
@@ -41,41 +57,46 @@ class _SoundWidgetState extends State<SoundWidget> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           GestureDetector(
-            onTap: _onIconPressed,
+            onTap: _onIconTapped,
             child: SvgPicture.asset(
               "assets/icons/${widget.asset}.svg",
-              color: _isActive ? Colors.indigo : Colors.black26,
+              color: _playing.value ? Colors.indigo : Colors.black26,
               width: 62,
               height: 62,
             ),
           ),
           Text(widget.title),
           Slider(
-            value: _volume,
-            onChanged: _isActive ? _onVolumeChanged : null,
-            max: 100,
+            value: _volume.value,
+            onChanged: _playing.value ? _onSliderChange : null,
+            max: 1.0,
             min: 0,
           )
         ],
       );
   }
 
-  void _onVolumeChanged(double vol) {
+  void _onIconTapped() {
     setState(() {
-      _volume = vol;
-      _player.setVolume(vol / 100);
+      _playing.value = !_playing.value;
     });
   }
 
-  void _onIconPressed() {
+  void _onSliderChange(double vol) {
     setState(() {
-      _isActive = !_isActive;
-
-      if (_isActive) {
-        _player.play(AssetSource("sounds/${widget.asset}.ogg"), volume: _volume / 100);
-      } else {
-        _player.stop();
-      }
+      _volume.value = vol;
     });
+  }
+  
+  void _startOrStopPlayback() {
+    if (_playing.value) {
+      _player.play(AssetSource("sounds/${widget.asset}.ogg"), volume: _volume.value);
+    } else {
+      _player.stop();
+    }
+  }
+
+  void _changePlaybackVolume() {
+    _player.setVolume(_volume.value);
   }
 }
